c4516f41599b202a3ba40a3c348ec3a02821fe9d
ednachavez1510-blip <ednachavez1510@gmail.com>
2025-09-26 20:44:01 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,7 @@ const citas = [
 ];
 
 // Definimos la función que mostrará una cita aleatoria
-function generarCita() {
-  // Generamos un número aleatorio entre 0 y la cantidad de frases (citas.length)
-  // Math.random() → número entre 0 y 1
-  // * citas.length → escala ese número al rango de frases disponibles
-  // Math.floor() → redondea hacia abajo para obtener un número entero válido
+function generarCancion() {
   const indice = Math.floor(Math.random() * citas.length);
-
-  // Buscamos en el documento el elemento con id="quote"
-  // Cambiamos su contenido de texto por la frase que corresponde al índice generado
   document.getElementById("quote").innerText = citas[indice];
 }
-
-
